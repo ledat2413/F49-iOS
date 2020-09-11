88b5db28245581b234c12208089d0202eaf016b9
Le Dat <datle2413@gmail.com>
2020-09-11 09:22:09 +0700

    datle commit

---
+++ F49/Helpers/ExtensionUIView.swift
@@ -0,0 +1,25 @@
+//
+//  ExtensionUIView.swift
+//  F49
+//
+//  Created by Le Dat on 9/11/20.
+//  Copyright © 2020 Le Dat. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    func setGradientBackground(colorOne: UIColor, colorTwo: UIColor) {
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.0)
+        
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
