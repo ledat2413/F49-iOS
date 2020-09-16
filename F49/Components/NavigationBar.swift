b84da2967374d538b71fd832c615f5b3cab44fb8
Le Dat <datle2413@gmail.com>
2020-09-16 09:13:33 +0700

    commit 16/9

---
+++ F49/Components/NavigationBar.swift
@@ -0,0 +1,48 @@
+//
+//  NavigationBar.swift
+//  F49
+//
+//  Created by Le Dat on 9/16/20.
+//  Copyright © 2020 Le Dat. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class NavigationBar: UIView{
+    
+    private static let NIB_NAME = "NavigationBar"
+    
+    @IBOutlet var view: UIView!
+    @IBOutlet weak var backGroundImageView: UIImageView!
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var title: String = "" {
+        didSet {
+            titleLabel.text = title
+        }
+    }
+    
+    
+    override func awakeFromNib() {
+        initWithNib()
+    }
+    
+    private func initWithNib() {
+        Bundle.main.loadNibNamed(NavigationBar.NIB_NAME, owner: self, options: nil)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(view)
+        setupLayout()
+    }
+    private func setupLayout() {
+        NSLayoutConstraint.activate(
+            [
+                view.topAnchor.constraint(equalTo: topAnchor),
+                view.leadingAnchor.constraint(equalTo: leadingAnchor),
+                view.bottomAnchor.constraint(equalTo: bottomAnchor),
+                view.trailingAnchor.constraint(equalTo: trailingAnchor),
+            ]
+        )
+    }
+}
